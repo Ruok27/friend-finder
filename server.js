fbd675e87842b6c2b135c799ed32587c0563b900
ruok27 <jakxii27@gmail.com>
2019-12-23 21:07:49 -0500

    established local server

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
-const express = require("express");
-const path = require("path");
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+
+
+const PORT = 8080;
+
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/app/public/home.html'));
+});
+
+app.listen(PORT, function() {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
+
+
