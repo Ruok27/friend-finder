fcf07f655d6a87f8fc0d353cb0d37c9e54513c33
ruok27 <jakxii27@gmail.com>
2019-12-24 04:08:19 -0500

    htmlRoutes were established

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 let express = require('express');
 let app = express();
 let path = require('path');
-
-
-
-
 var PORT = process.env.PORT || 3000;
+const htmlroute = require("./app/routing/htmlRoutes.js");
+
+app.use(htmlroute);
 
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/app/public/home.html'));
-});
 
 app.listen(PORT, function() {
   console.log(`Server listening on: http://localhost:${PORT}`);
