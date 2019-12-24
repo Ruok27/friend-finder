d9d8a9c274ce3f59811ba49279cf06eb2a4119db
ruok27 <jakxii27@gmail.com>
2019-12-23 21:23:29 -0500

    json file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+let express = require('express');
+let app = express();
+let path = require('path');
+let mysql = require('mysql');
 
 
 
